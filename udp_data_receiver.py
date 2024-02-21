1facbb3b47a2cf8258a1bad06874c3a52e203e49
evgenyagantaev <agantaev@mail.ru>
2024-02-21 11:31:25 +0300

    Add project files.

---
+++ udp_data_receiver.py
@@ -0,0 +1,30 @@
+﻿import socket
+import time
+
+relay_host = "45.135.233.152"
+relay_port = 11373
+
+receiver_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+
+def send_data_to_relay(message):
+    global relay_host, relay_port, receiver_socket
+    
+    receiver_socket.sendto(message.encode(), (relay_host, relay_port))
+    print(f"Sent message to relay: {message}")
+    
+def receive_data_from_relay():
+    while True:
+        data, addr = receiver_socket.recvfrom(1024)  # Получаем данные
+        print(f"Received message from {addr}: {data.decode()}")
+        
+        
+
+if __name__ == "__main__":
+
+    # Отправка тестового сообщения
+    send_data_to_relay("Hello from Receiver!")
+    
+    receive_data_from_relay()
+        
+
---
+++ udp_data_sender.py
@@ -0,0 +1,27 @@
+﻿import socket
+import time
+
+relay_host = "45.135.233.152"
+relay_port = 11373
+
+sender_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+
+def send_data_to_relay(message):
+    global relay_host, relay_port, sender_socket
+    
+    sender_socket.sendto(message.encode(), (relay_host, relay_port))
+    print(f"Sent message to relay: {message}")
+    
+
+if __name__ == "__main__":
+    
+
+    # Отправка тестового сообщения
+    send_data_to_relay("Hello from Sender!")
+
+    # Отправка текущего времени в цикле с периодом в 1 секунду
+    while True:
+        current_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+        send_data_to_relay(f"Current time: {current_time}")
+        time.sleep(1)
