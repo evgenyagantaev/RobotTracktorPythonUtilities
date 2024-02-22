012dce0eea8d2543138fdaf0f01fdfcd10239ce3
evgenyagantaev <agantaev@mail.ru>
2024-02-22 15:27:50 +0300

    moved from test to real 256*4*4 datagramms;

--- udp_data_receiver.py
+++ udp_data_receiver.py
@@ -1,5 +1,6 @@
 ﻿import socket
 import time
+import struct
 
 relay_host = "45.135.233.152"
 relay_port = 11373
@@ -15,9 +16,16 @@ def send_data_to_relay(message):
     
 def receive_data_from_relay():
     while True:
-        data, addr = receiver_socket.recvfrom(1024)  # Получаем данные
-        print(f"Received message from {addr}: {data.decode()}")
+        data, addr = receiver_socket.recvfrom(256*4*4)  # Получаем данные
+        print(f"Received message from {addr}: {data.size()} bytes")
+        received_buffer = deserialize_float_array(data, 256, 4)
         
+def deserialize_float_array(data, width, height):
+    # Размер float в байтах: 4 байта (для формата 'f' в структуре)
+    float_size = 4
+    num_floats = width * height
+    floats = struct.unpack(f'{num_floats}f', data)
+    return [floats[i * width:(i + 1) * width] for i in range(height)]
         
 
 if __name__ == "__main__":
